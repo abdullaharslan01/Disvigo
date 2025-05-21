c1ce9cb678701c13a3753d9302ca1d0a24cae0af
Abdullah <abdullaharslan1473@gmail.com>
2025-05-21 16:03:52 +0300

    Initial Commit

---
+++ Disvigo/DisvigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisvigoApp.swift
+//  Disvigo
+//
+//  Created by abdullah on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct DisvigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
